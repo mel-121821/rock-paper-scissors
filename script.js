21f1a92ee22ce2dca105c5974c795ad89469963e
mel_121821 <meganlindsay48@gmail.com>
2022-06-20 07:48:42 -0400

    Create file and move content over from js.script

---
+++ script.js
@@ -0,0 +1,14 @@
+// Project: to build a rock paper scissors game //
+
+// Does your program have a user interface? No (will be coming later)
+// What inputs will your program have? User will enter 1 of 3 options and will be given an error message if input is something outside of these options
+// What is the desired output? A "You win" or "You lose" depending on choices by user and computer
+// Given your inputs, what are the steps necessary to return the desired output? See plan below: 
+
+// Plan: User selects 1 of 3 options, rock, paper and scissors and stores the result in a variable. The computer generates a random selection of the same 3 options and stores it in another variable. If the user and computer pick the same option, print "Tie game." Remaining matchups below: //
+
+// If user picks rock and computer picks scissors, print "You win" //
+// If user picks rock and computer picks paper, print "You lose" //
+// If user picks paper and computer picks rock, print "You win" //
+// If user picks paper and computer picks scissors, print "You lose" //
+// If user picks scissors and computer picks paper, print "You win" //
